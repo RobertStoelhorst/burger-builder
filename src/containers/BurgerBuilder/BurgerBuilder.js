737a09796e858dba1a57401a53a1ab222b58d26f
RobertStoelhorst <robbiestoelhorst@hotmail.com>
2021-02-28 20:38:45 +1100

    successful order post to firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -81,7 +82,25 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("You continue!");
+    // alert("You continue!");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice, // IMPORTANT Note in a real world app you would recalculate the price on the server as to avoid a user sending there own code to manipulate the price...!!!
+      customer: {
+        name: "Rob Stolly",
+        address: {
+          street: "Street test",
+          postCode: "2000",
+          country: "Australia",
+        },
+        email: "test@test.com",
+      },
+      deliveryMethod: "fastest",
+    };
+    axios
+      .post("/orders.json", order) // for sending orders to the order database
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
   };
 
   render() {
